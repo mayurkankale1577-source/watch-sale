0caa589aa98d8d38ecb0559d7d67ec7c2c63ab31
Mayur Kankale <MayurK@sycasol>
2026-03-24 18:28:20 +0530

    db file updatd

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,15 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "watch_sales"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 export default db;
+
+
